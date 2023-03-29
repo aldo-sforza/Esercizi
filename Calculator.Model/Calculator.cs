1f65f8e9ff73b3f7204f633b85253d96efc26c0a
Aldo Sforza <aldo.sforza@its-ictpiemonte.it>
2023-03-29 18:00:23 +0200

    add test

---
+++ Calculator.Model/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Model
+{
+    public class Calculator
+    {
+        public object Add(int v1, int v2)
+        {
+            return v1+v2;
+        }
+    }
+}
---
+++ Calculator.Tests/CalculatorTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTwoNumberSuccess()
+        {
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            var actualResult =calculator.Add(1,2);
+
+            //assert
+            actualResult.Should().Be(3);
+
+        }
+
+        [Fact]
+        public void AddTwoNumberSuccessAgain()
+        {
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            var actualResult = calculator.Add(2, 2);
+
+            //assert
+            actualResult.Should().Be(4);
+
+        }
+    }
+}
