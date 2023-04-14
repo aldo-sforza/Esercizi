639e7abe453036f8fbac926738dd582677459001
Luca <luca.ronca.lr@gmail.com>
2023-04-14 22:09:51 +0200

    ShapesRepository - Implemented IShapesRepository methods

--- Shapes.Model/ShapesRepository.cs
+++ Shapes.Model/ShapesRepository.cs
@@ -8,28 +8,27 @@ using System.Threading.Tasks;
 
 namespace Shapes.Model
 {
-    public class ShapesRepository : IRepository<Shape>, IShapesQuery
+    public class ShapesRepository : IShapesRepository, IRepository<Shape>, IShapesQuery
     {
         private readonly Dictionary<string, Shape> _shapes = new();
-        public ShapesRepository()
+        public ShapesRepository() 
         {
-            
-        }
 
+        }
+        
+        //IShapeRepository
+        public void CreateRectangle(string id, double width, double height)
+            =>_shapes.Add(id, new Rectangle(width, height, id));
+        public void CreateSquare(string id, double edge)
+            =>_shapes.Add(id, new Square(edge, id));
+        public void CreateCircle(string id, double radius)
+            =>_shapes.Add(id, new Square(radius, id));
+
+        
+        //IRepository
         public void Create(string id)
         {
             throw new NotImplementedException();
-        }
-        public void Create(string id, double width, double height)
-        {
-            if (width.Equals(height))
-                _shapes.Add(id, new Square(width, id));
-            else
-                _shapes.Add(id, new Rectangle(width, height, id));
-        }
-        public void Create (string id, double radius)
-        {
-            _shapes.Add(id, new Circle(radius, id));
         }
         public void Delete(string id)
         {
@@ -39,40 +38,56 @@ namespace Shapes.Model
         {
             return _shapes.ContainsKey(id);
         }
-
-        public Circle GetCircle(string id)
-        {
-            var filter = new Circle(1, "filter");
-            if (!_shapes[id].GetType().Equals(filter.GetType()))
-                return null;
-            else
-                return (Circle)_shapes[id];
-        }
-
-        public Rectangle GetRectangle(string id)
-        {
-            var filter = new Rectangle(2, 1, "filter");
-            if (!_shapes[id].GetType().Equals(filter.GetType()))
-                return null;
-            else
-                return (Rectangle)_shapes[id];
-        }
-
-        public Square GetSquare(string id)
-        {
-            var filter = new Square(1, "filter");
-            if (!_shapes[id].GetType().Equals(filter.GetType()))
-                return null;
-            else
-                return (Square)_shapes[id];
-        }
-
         public Shape Load(string id)
         {
             if (_shapes.ContainsKey(id))
                 return _shapes[id];
 
             return null;
+        }
+
+        //IShapesQuery
+        public bool TryGetCircle(string id, out Circle circle)
+        {
+            circle = null;
+            if (!_shapes[id].GetType().Equals(typeof(Circle)))
+            {
+                circle = (Circle)_shapes[id];
+                return true; 
+            }
+            return false;
+        }
+        public IEnumerable<Circle> GetCircles()
+        {
+            return _shapes.Values.OfType<Circle>();
+        }
+        public bool TryGetRectangle(string id, out Rectangle rectangle)
+        {
+            rectangle = null;
+            if (!_shapes[id].GetType().Equals(typeof(Circle)))
+            {
+                rectangle = (Rectangle)_shapes[id];
+                return true;
+            }
+            return false;
+        }
+        public IEnumerable<Rectangle> GetRectangles()
+        {
+            return _shapes.Values.OfType<Rectangle>();
+        }
+        public bool TryGetSquare(string id, out Square square)
+        {
+            square = null;
+            if (!_shapes[id].GetType().Equals(typeof(Circle)))
+            {
+                square = (Square)_shapes[id];
+                return true;
+            }
+            return false;
+        }
+        public IEnumerable<Square> GetSquares()
+        {
+            return _shapes.Values.OfType<Square>();
         }
     }
 }
