3bb578e004c69ffbb0041187c894a97a8527da51
Tesla <42157002+P4ulTesla@users.noreply.github.com>
2023-04-14 17:25:08 +0200

    Repository Shape Implementato

--- Shapes.Model/ShapeRepository.cs
+++ Shapes.Model/ShapeRepository.cs
@@ -7,26 +7,70 @@ using System.Threading.Tasks;
 
 namespace Shapes.Model
 {
-    public class ShapeRepository : IRepository<Shape>
+    public class RealShape : Shape
     {
-        public void Create(string id)
+        public RealShape(string id) : base(id)
+        {
+        }
+
+        public override double CalculatePerimeter()
         {
             throw new NotImplementedException();
+        }
+
+        public override double CalculateSurface()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public interface IShapeQuery : IUnitOfWork
+    {
+    }
+    public class ShapeRepository : IRepository<RealShape>, IShapeQuery
+    {
+        private const string FILE_NAME = "shapes.txt";
+        private readonly Dictionary<string, RealShape> _shapes = new();
+
+        public ShapeRepository()
+        {
+            var folder = AppDomain.CurrentDomain.BaseDirectory;
+            var fullPath = System.IO.Path.Combine(folder, FILE_NAME);
+            if (File.Exists(fullPath))
+            {
+                var content = File.ReadAllText(fullPath);
+                _shapes = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, RealShape>>(content);
+            }
+        }
+        public void Create(string id)
+        {
+            var shape = new RealShape(id);
+            _shapes.Add(shape.Id, shape);
         }
 
         public void Delete(string id)
         {
-            throw new NotImplementedException();
+            _shapes.Remove(id);
         }
 
         public bool Exists(string id)
         {
-            throw new NotImplementedException();
+            return _shapes.ContainsKey(id);
         }
 
-        public Shape Load(string id)
+        public RealShape Load(string id)
         {
-            throw new NotImplementedException();
+            if (_shapes.ContainsKey(id))
+                return _shapes[id];
+
+            return null;
+        }
+
+        public void SaveChanges()
+        {
+            var folder = AppDomain.CurrentDomain.BaseDirectory;
+            var fullPath = System.IO.Path.Combine(folder, FILE_NAME);
+            var content = System.Text.Json.JsonSerializer.Serialize(_shapes);
+            System.IO.File.WriteAllText(fullPath, content);
         }
     }
 }
