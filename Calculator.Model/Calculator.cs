dc320baeb5387dbe7b71f06ef27b64891d0c1066
genesi-o <matteo.genesio@edu.itspiemonte.it>
2023-04-14 15:26:22 +0200

    subtract operation added

--- Calculator.Model/Calculator.cs
+++ Calculator.Model/Calculator.cs
@@ -5,5 +5,8 @@
         public int Add(int v1, int v2) => v1+ v2;
 
         public double Add(double v1, double v2) => v1+v2;
+
+        public int Sub(int a1, int a2) => a1-a2;
+        
     }
 }
