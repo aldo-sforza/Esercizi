07f87dc8d53aa7c7c47b236fe523f0df10111377
Tesla <42157002+P4ulTesla@users.noreply.github.com>
2023-04-14 15:26:34 +0200

    Substract, Multiply, Divide

--- Calculator.Model/Calculator.cs
+++ Calculator.Model/Calculator.cs
@@ -7,5 +7,10 @@
             return v1+v2;
         }
         public double Add(double v1, double v2) => v2 + v2;
+        public double Substract(double v1, double v2) => v2 - v1;
+
+        public double Multiply(double v1, double v2) => v1*v2;
+
+        public double Divide(double v1, double v2) => v1 / v2;
     }
 }
