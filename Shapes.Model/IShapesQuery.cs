324cb5c22cff57bb50f8351b7b2abd40f399aa71
Luca <luca.ronca.lr@gmail.com>
2023-04-14 16:44:30 +0200

    Implemented interface IShapesQuery

---
+++ Shapes.Model/IShapesQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Shapes.Model
+{
+    internal interface IShapeQuery
+    {
+        public Rectangle GetRectangle(string id);
+        public Square GetSquare(string id);
+        public Circle GetCircle(string id);
+    }
+}
--- Shapes.Model/ShapeRepository.cs
+++ Shapes.Model/ShapeRepository.cs
@@ -2,12 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shapes.Model
 {
-    public class ShapeRepository : IRepository<Shape>, IUnitOfWork
+    public class ShapeRepository : IRepository<Shape>, IUnitOfWork, IShapeQuery
     {
         public void Create(string id)
         {
