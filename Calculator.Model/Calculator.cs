e058d2de2afb5ce6a465ef9cc076500862a91019
Luca <luca.ronca.lr@gmail.com>
2023-04-14 15:23:27 +0200

    Calculator - implemented Sub, Multiply, Div metods

--- Calculator.Model/Calculator.cs
+++ Calculator.Model/Calculator.cs
@@ -4,8 +4,13 @@
     {
         public int Add(int v1, int v2)
            => v1+v2;
-
         public double Add(double v1, double v2)
             => v1+v2;
+        public int Sub(int v1, int v2)
+            => v1-v2;
+        public int Multiply(int v1, int v2)
+            => v1*v2;
+        public double Div(int v1, int v2)
+            => v1/v2;
     }
 }
