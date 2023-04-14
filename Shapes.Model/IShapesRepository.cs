03bed82fb15b2a5331b21b27585284d5ee16e1b4
Luca <luca.ronca.lr@gmail.com>
2023-04-14 22:02:00 +0200

    Shapes.Model - Implemented IShapeRepository

---
+++ Shapes.Model/IShapesRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Shapes.Model
+{
+    public interface IShapesRepository
+    {
+        public void CreateSquare(string id, double edge);
+        public void CreateRectangle(string id, double width, double height);
+        public void CreateCircle(string id, double radius);
+    }
+}
