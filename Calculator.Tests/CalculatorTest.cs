c0a1fd89f99ff5d2cc9bc6c7b08129cf3827fd3f
e_tavagnutti <enrico.tavagnutti@edu.itspiemonte.it>
2023-04-14 15:27:52 +0200

    add subtraction test

--- Calculator.Tests/CalculatorTest.cs
+++ Calculator.Tests/CalculatorTest.cs
@@ -41,6 +41,23 @@ namespace Calculator.Tests
 
         }
 
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 2, 4)]
+        [InlineData(2, 3, 5)]
+        [InlineData(2, 4, 6)]
+        public void SubtractTwoNumberSuccess(int a1, int a2, int result)
+        {
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            var actualResult = calculator.Subtract(a1, a2);
+
+            //assert
+            actualResult.Should().Be(result);
+
+        }
 
     }
 }
