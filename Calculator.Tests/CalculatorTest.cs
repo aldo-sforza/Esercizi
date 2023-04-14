0e15b567cc52a977275932ca687ff67271d7d11c
e_tavagnutti <enrico.tavagnutti@edu.itspiemonte.it>
2023-04-14 15:20:43 +0200

    add doubles test

--- Calculator.Tests/CalculatorTest.cs
+++ Calculator.Tests/CalculatorTest.cs
@@ -23,6 +23,24 @@ namespace Calculator.Tests
 
         }
 
-       
+        [Theory]
+        [InlineData(1.1, 2.2, 3.3)]
+        [InlineData(2.2, 2.2, 4.4)]
+        [InlineData(2.2, 3.3, 5.5)]
+        [InlineData(2.2, 4.4, 6.6)]
+        public void AddTwoDoublesSuccess(double a1, double a2, double result)
+        {
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            var actualResult = calculator.AddDouble(a1, a2);
+
+            //assert
+            actualResult.Should().Be(result);
+
+        }
+
+
     }
 }
