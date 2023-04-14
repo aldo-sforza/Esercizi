59f4518a0a1f2bdde66dcf839ebe365a9973d869
genesi-o <matteo.genesio@edu.itspiemonte.it>
2023-04-14 15:26:44 +0200

    test added

--- Calculator.Tests/CalculatorTest.cs
+++ Calculator.Tests/CalculatorTest.cs
@@ -23,6 +23,35 @@ namespace Calculator.Tests
 
         }
 
-       
+        [Theory]
+        [InlineData(1.1, 2.2, 3.3)]
+        [InlineData(2.2, 2.3, 4.5)]
+        public void AddTwoDoubleNumbersucces(double a1, double a2, double result)
+        {
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            var actualResult = calculator.Add(a1,a2);
+
+            //assert
+            actualResult.Should().Be(result);
+        }
+
+        [Theory]
+        [InlineData(1, 2, -1)]
+        [InlineData(2, 3, -1)]
+        [InlineData(6, 3, 3)]
+        public void SubTwoNumbersucces(int a1, int a2, int result)
+        {
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            var actualResult = calculator.Sub(a1,a2);
+
+            //assert
+            actualResult.Should().Be(result);
+        }
     }
 }
