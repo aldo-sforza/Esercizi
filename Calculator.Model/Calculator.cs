0a2e80de7286190c4c6e161cc2a51bfac365ea33
e_tavagnutti <enrico.tavagnutti@edu.itspiemonte.it>
2023-04-14 15:27:39 +0200

    add "subtraction" method

--- Calculator.Model/Calculator.cs
+++ Calculator.Model/Calculator.cs
@@ -8,6 +8,9 @@
         }
 
         public double AddDouble(double d1, double d2)
-            => d1 + d2; 
+            => d1 + d2;
+
+        public double Subtract(int s1, int s2)
+            => s1 - s2;
     }
 }
