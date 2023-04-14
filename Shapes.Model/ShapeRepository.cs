b22f258e87364772aacc55bc932fad983b918f9e
MarcheseAndrea <marchese.andrea19@studentipinin.it>
2023-04-14 16:41:14 +0200

    Created IRepositroy for multiple types

---
+++ Shapes.Model/ShapeRepository.cs
@@ -0,0 +1,32 @@
+﻿using Patterns.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Model
+{
+    internal class ShapeRepository : IRepository<Shape>
+    {
+        public void Create(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Shape Load(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
