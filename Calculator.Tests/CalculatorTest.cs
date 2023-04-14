9f019a50be79dacb8047f918bf04cf0c563c4a11
Aldo Sforza <aldo.sforza@its-ictpiemonte.it>
2023-04-14 15:25:58 +0200

    add tests

--- Calculator.Tests/CalculatorTest.cs
+++ Calculator.Tests/CalculatorTest.cs
@@ -23,6 +23,38 @@ namespace Calculator.Tests
 
         }
 
-       
+        [Theory]
+        [InlineData(1.1, 2.2, 3.3)]
+        [InlineData(2.1, 2.1, 4.2)]
+        public void AddTwoDoubleNumberSuccess(double a1, double a2, double result)
+        {
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            var actualResult = calculator.Add(a1, a2);
+
+            //assert
+            actualResult.Should().Be(result);
+
+        }
+
+        [Theory]
+        [InlineData(1,2,-1)]
+        [InlineData(2,2,0)]
+        public void SubtractOf(int a1,int a2, int result)
+        {
+
+            //arrange
+            var calculator = new Model.Calculator();
+
+            //act
+            int actualResult = calculator.Subtract(a1, a2);
+
+            //assert
+            actualResult.Should().Be(result);
+        }
+
+
     }
 }
