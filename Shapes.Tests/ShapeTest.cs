d6db58ed2d49781cc23638b002983a0caa8ba0d8
marchebarcheITS <119409273+marchebarcheITS@users.noreply.github.com>
2023-04-16 17:24:22 +0200

    Create Success for circle, square and rectangle

--- Shapes.Tests/ShapeTest.cs
+++ Shapes.Tests/ShapeTest.cs
@@ -10,5 +10,42 @@ namespace Shapes.Tests
     {
         private readonly MockShapeRepository _shapeRepository = new MockShapeRepository();
 
-        
+        [Fact]
+        public void CreateCircleSuccess()
+        {
+            //Arrange
+            var shapeApplicationService = new ShapeApplicationService(_shapeRepository.Object);
+
+            //Act
+            shapeApplicationService.HandleCommand(new CreateCircle("123", 3));
+            //Assert
+            _shapeRepository.GivenCreateShapeCircleTimes(Times.Once());
+            _shapeRepository.GivenCreateShapeRectangleTimes(Times.Never());
+            _shapeRepository.GivenCreateShapeSquareTimes(Times.Never());
+        }
+        [Fact]
+        public void CreateRectangleSuccess() 
+        {
+            var shapeAplicationService = new ShapeApplicationService(_shapeRepository.Object);
+
+            shapeAplicationService.HandleCommand(new CreateRectangle("123", 2, 5));
+            
+            _shapeRepository.GivenCreateShapeRectangleTimes(Times.Once());
+            _shapeRepository.GivenCreateShapeCircleTimes(Times.Never());
+            _shapeRepository.GivenCreateShapeSquareTimes(Times.Never());
+        }
+
+        [Fact]
+        public void CreateSquareSuccess()
+        {
+            var shapeApplicationService = new ShapeApplicationService(_shapeRepository.Object);
+            
+            shapeApplicationService.HandleCommand(new CreateSquare("123", 3));
+
+            _shapeRepository.GivenCreateShapeSquareTimes(Times.Once());
+            _shapeRepository.GivenCreateShapeCircleTimes(Times.Never());
+            _shapeRepository.GivenCreateShapeRectangleTimes(Times.Never());
+
+        }
+    } 
 }
